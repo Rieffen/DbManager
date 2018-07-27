a67eea54ca55d3182315644fcf0d476442877735
Ronan Giron <ElGigi@users.noreply.github.com>
2018-07-28 01:31:02 +0200

    Add PDO methods in DriverInterface PhpDoc

--- src/Driver/DriverInterface.php
+++ src/Driver/DriverInterface.php
@@ -12,7 +12,24 @@
 
 namespace Berlioz\DbManager\Driver;
 
-
+/**
+ * Interface DriverInterface
+ *
+ * @package Berlioz\DbManager\Driver
+ *
+ * @method mixed errorCode() Fetch the SQLSTATE associated with the last operation on the database handle
+ * @method array errorInfo() Fetch extended error information associated with the last operation on the database handle
+ * @method int exec(string $statement) Execute an SQL statement and return the number of affected rows
+ * @method mixed getAttribute(int $attribute) Retrieve a database connection attribute
+ * @method bool inTransaction() Checks if inside a transaction
+ * @method string lastInsertId(string $name = null) Returns the ID of the last inserted row or sequence value
+ * @method \PDOStatement prepare(string $statement, array $driver_options = []) Prepares a statement for execution and
+ *         returns a statement object
+ * @method \PDOStatement query(string $statement, int $fetch_mode = \PDO::FETCH_COLUMN) Executes an SQL statement,
+ *         returning a result set as a \PDOStatement object
+ * @method string quote(string $string, int $parameter_type = \PDO::PARAM_STR) Quotes a string for use in a query
+ * @method bool setAttribute(int $attribute, mixed $value) Set an attribute
+ */
 interface DriverInterface
 {
     /**
